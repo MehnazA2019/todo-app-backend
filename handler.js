ff55c096a4806aed5aeba9e4e6bd86390477c46d
Mehnaz <mehnazahmed1122@gmail.com>
2020-03-03 20:27:39 +0000

    adding_three_funtions

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.tasks = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -16,3 +16,4 @@ module.exports.hello = async event => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
